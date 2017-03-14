c76e045d6ecf1a61c740ba24864286cae11f2496
daniel <dasdo1@gmail.com>
2017-03-13 20:29:16 -0400

    add image manipulate form template processor

--- src/PhpWord/TemplateProcessor.php
+++ src/PhpWord/TemplateProcessor.php
@@ -99,6 +99,8 @@ class TemplateProcessor
             $index++;
         }
         $this->tempDocumentMainPart = $this->fixBrokenMacros($this->zipClass->getFromName('word/document.xml'));
+
+        $this->_countRels = 100; //start id for relationship between image and document.xml
     }
 
     /**
@@ -323,6 +325,13 @@ class TemplateProcessor
 
         $this->zipClass->addFromString('word/document.xml', $this->tempDocumentMainPart);
 
+        if ($this->_rels != "") {
+            $this->zipClass->addFromString('word/_rels/document.xml.rels', $this->_rels);
+        }
+        if ($this->_types != "") {
+            $this->zipClass->addFromString('[Content_Types].xml', $this->_types);
+        }
+
         foreach ($this->tempDocumentFooters as $index => $headerXML) {
             $this->zipClass->addFromString($this->getFooterName($index), $this->tempDocumentFooters[$index]);
         }
@@ -505,5 +514,75 @@ class TemplateProcessor
         }
 
         return substr($this->tempDocumentMainPart, $startPosition, ($endPosition - $startPosition));
+    }
+
+    public function setImg($strKey, $img)
+    {
+        $strKey = '${' . $strKey . '}';
+        $relationTmpl = '<Relationship Id="RID" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/IMG"/>';
+
+        $imgTmpl = '<w:pict><v:shape type="#_x0000_t75" style="width:WIDpx;height:HEIpx"><v:imagedata r:id="RID" o:title=""/></v:shape></w:pict>';
+
+        $toAdd = $toAddImg = $toAddType = '';
+        $aSearch = array('RID', 'IMG');
+        $aSearchType = array('IMG', 'EXT');
+        $countrels = $this->_countRels++;
+        //I'm work for jpg files, if you are working with other images types -> Write conditions here
+        $imgExt = 'jpg';
+        $imgName = 'img' . $countrels . '.' . $imgExt;
+
+        $this->zipClass->deleteName('word/media/' . $imgName);
+        $this->zipClass->addFile($img['src'], 'word/media/' . $imgName);
+
+        $typeTmpl = '<Override PartName="/word/media/' . $imgName . '" ContentType="image/EXT"/>';
+
+        $rid = 'rId' . $countrels;
+        $countrels++;
+        list($w, $h) = getimagesize($img['src']);
+
+        if (isset($img['swh'])) //Image proportionally larger side
+        {
+            if ($w <= $h) {
+                $ht = (int) $img['swh'];
+                $ot = $w / $h;
+                $wh = (int) $img['swh'] * $ot;
+                $wh = round($wh);
+            }
+            if ($w >= $h) {
+                $wh = (int) $img['swh'];
+                $ot = $h / $w;
+                $ht = (int) $img['swh'] * $ot;
+                $ht = round($ht);
+            }
+            $w = $wh;
+            $h = $ht;
+        }
+
+        if (isset($img['size'])) {
+            $w = $img['size'][0];
+            $h = $img['size'][1];
+        }
+
+        $toAddImg .= str_replace(array('RID', 'WID', 'HEI'), array($rid, $w, $h), $imgTmpl);
+        if (isset($img['dataImg'])) {
+            $toAddImg .= '<w:br/><w:t>' . $this->limpiarString($img['dataImg']) . '</w:t><w:br/>';
+        }
+
+        $aReplace = array($imgName, $imgExt);
+        $toAddType .= str_replace($aSearchType, $aReplace, $typeTmpl);
+
+        $aReplace = array($rid, $imgName);
+        $toAdd .= str_replace($aSearch, $aReplace, $relationTmpl);
+
+        $this->tempDocumentMainPart = str_replace('<w:t>' . $strKey . '</w:t>', $toAddImg, $this->tempDocumentMainPart);
+        //print $this->tempDocumentMainPart;
+
+        if ($this->_rels == "") {
+            $this->_rels = $this->zipClass->getFromName('word/_rels/document.xml.rels');
+            $this->_types = $this->zipClass->getFromName('[Content_Types].xml');
+        }
+
+        $this->_types = str_replace('</Types>', $toAddType, $this->_types) . '</Types>';
+        $this->_rels = str_replace('</Relationships>', $toAdd, $this->_rels) . '</Relationships>';
     }
 }
